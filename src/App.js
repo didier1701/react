5e42d3fc2a6111e2d06b5c2b2afec96a21fffd36
IMANIRAHARI Didier <didier53053@gmail.com>
2024-08-22 11:56:56 +0200

    next to talwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import './App.css';
-import Employee from './components/Employee';
-
+import "./App.css";
+import Employee from "./components/Employee";
+import {useState} from 'react';
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployee = true;
   return (
     <div className="App">
-      
       {showEmployee ? (
         <>
-        <Employee name="Magentle" role="intern" />
-        <Employee/>
-        <Employee/>
-        <Employee/>
-       </>
+          <input
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value)
+            }}
+          />
+          <Employee name="Magentle" role="intern" />
+          <Employee name="IMANIRAHARI" />
+          <Employee name = 'Abby' role = {role} />
+          <Employee />
+        </>
       ) : (
         <p>you can not see employees</p>
       )}
-      
-       
-        
-       
     </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,12 +1,10 @@
-function Employee(props){
-     return (
-          <>
-     <h3>Employee {props.name} </h3>
-     <p>{props.role ? props.role : "no role"} </p>
-
-     </>
-     );
-     
-     
+function Employee(props) {
+  return (
+    <>
+      <h3>Employee {props.name} </h3>
+      <p>{props.role ? props.role : "No role"} </p>
+      
+    </>
+  );
 }
 export default Employee;
