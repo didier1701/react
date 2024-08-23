f18fb6e15f5418c8aa957db92a5c6e63682ebc45
IMANIRAHARI Didier <didier53053@gmail.com>
2024-08-23 18:28:14 +0200

    next to some backend stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import "./App.css";
+import "./index.css";
 import Employee from "./components/Employee";
-import {useState} from 'react';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 function App() {
-  const [role, setRole] = useState('dev');
+  const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name: "MaGentle",
+        role: "devicePixelRatio",
+        image: "https://render.fineartamerica.com/images/rendered/default/framed-print/images/artworkimages/medium/2/1-elvis-presley-on-milton-berle-michael-ochs-archives.jpg?imgWI=7.5&imgHI=8&sku=CRQ13&mat1=PM918&mat2=&t=2&b=2&l=2&r=2&off=0.5&frameW=0.875"
+      },
+      {
+        name: "MaGentle",
+        role: "devicePixelRatio",
+        image: "https://render.fineartamerica.com/images/rendered/default/framed-print/images/artworkimages/medium/2/1-elvis-presley-on-milton-berle-michael-ochs-archives.jpg?imgWI=7.5&imgHI=8&sku=CRQ13&mat1=PM918&mat2=&t=2&b=2&l=2&r=2&off=0.5&frameW=0.875"
+      },
+      {
+        name: "MaGentle",
+        role: "devicePixelRatio",
+        image: "https://render.fineartamerica.com/images/rendered/default/framed-print/images/artworkimages/medium/2/1-elvis-presley-on-milton-berle-michael-ochs-archives.jpg?imgWI=7.5&imgHI=8&sku=CRQ13&mat1=PM918&mat2=&t=2&b=2&l=2&r=2&off=0.5&frameW=0.875"
+      },
+      {
+        name: "MaGentle",
+        role: "devicePixelRatio",
+        image: "https://render.fineartamerica.com/images/rendered/default/framed-print/images/artworkimages/medium/2/1-elvis-presley-on-milton-berle-michael-ochs-archives.jpg?imgWI=7.5&imgHI=8&sku=CRQ13&mat1=PM918&mat2=&t=2&b=2&l=2&r=2&off=0.5&frameW=0.875"
+      },
+      {
+        name: "MaGentle",
+        role: "devicePixelRatio",
+        image: "https://render.fineartamerica.com/images/rendered/default/framed-print/images/artworkimages/medium/2/1-elvis-presley-on-milton-berle-michael-ochs-archives.jpg?imgWI=7.5&imgHI=8&sku=CRQ13&mat1=PM918&mat2=&t=2&b=2&l=2&r=2&off=0.5&frameW=0.875"
+      },
+      {
+        name: "MaGentle",
+        role: "devicePixelRatio",
+        image: "https://render.fineartamerica.com/images/rendered/default/framed-print/images/artworkimages/medium/2/1-elvis-presley-on-milton-berle-michael-ochs-archives.jpg?imgWI=7.5&imgHI=8&sku=CRQ13&mat1=PM918&mat2=&t=2&b=2&l=2&r=2&off=0.5&frameW=0.875"
+      }
+    ]
+  );
   const showEmployee = true;
   return (
     <div className="App">
@@ -11,13 +46,21 @@ function App() {
           <input
             type="text"
             onChange={(e) => {
-              setRole(e.target.value)
+              setRole(e.target.value);
             }}
           />
-          <Employee name="Magentle" role="intern" />
-          <Employee name="IMANIRAHARI" />
-          <Employee name = 'Abby' role = {role} />
-          <Employee />
+          <div className="flex flex-wrap justify-center">
+            {employees.map( (employee) => {
+              return (
+                <Employee
+                  key= {uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  image={employee.image}
+                />
+            );
+            })}
+          </div>
         </>
       ) : (
         <p>you can not see employees</p>
